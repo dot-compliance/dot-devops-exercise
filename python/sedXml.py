d524c0b422e46040226f49fd2231b74df9d5d12c
Hagit Segev <hagit.segev4@gmail.com>
2024-01-24 11:29:49 +0200

    wip

--- python/sedXml.py
+++ python/sedXml.py
@@ -1,1 +1,17 @@
 print('Hello, World!')
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-f", "--file_to_sed", dest="file_to_sed", default="manifest\\package1.xml",
+                    help="File to edit")
+parser.add_argument("-o", "--orig_string", dest="orig_string", default="PLACE_HOLDER",
+                    help="What string to replace")
+parser.add_argument("-n", "--new_string", dest="new_string", default="HelloWorld.cls",
+                    help="The new string to set")
+
+args = parser.parse_args()
+with open(args.file_to_sed,'r') as file:
+    filedata = file.read()
+    filedata = filedata.replace(args.orig_string, args.new_string)
+with open(args.file_to_sed,'w') as file:
+    file.write(filedata)
